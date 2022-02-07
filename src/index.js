18380ed48ebbb1656f5b5cdde72073ccfb6db855
Juan Manuel Fernandez <82913468+juanmf1@users.noreply.github.com>
2022-02-07 14:55:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // HAY QUE PONERLO ARRIBA PORQUE SINO NO LEE JSON EN LAS RUTAS DE ABAJO
 
